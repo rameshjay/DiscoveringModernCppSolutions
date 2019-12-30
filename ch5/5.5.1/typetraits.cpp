234ecf9e64d22ed1bfde7ec1d206a8ab15f63697
Adem Budak <admbudk@gmail.com>
2019-12-30 07:06:39 +0300

    [Added] Solution for first part of exercise 5.5.1

---
+++ ch5/5.5.1/typetraits.cpp
@@ -0,0 +1,37 @@
+#include <type_traits>
+#include <iostream>
+
+namespace mine {
+
+template <typename T>
+struct remove_reference {
+  using type = T;
+};
+
+template <typename T>
+struct remove_reference<T &> {
+  using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+template <typename T>
+struct add_reference {
+  using type = T &;
+};
+
+template <typename T>
+using add_reference_t = typename add_reference<T>::type;
+
+template <typename T, typename U>
+bool is_same_v = std::is_same<T, U>::value;
+
+} // namespace mine
+
+int main() {
+  using namespace mine;
+
+  std::cout << is_same_v<remove_reference_t<const int &>, const int>;
+  std::cout << is_same_v<add_reference_t<int>, int &>;
+}
